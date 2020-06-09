fb52be43206319dff7b676b410c3fd0a3a23b323
Mariano Campos <38134810+MarianC312@users.noreply.github.com>
2020-06-08 23:35:16 -0300

    update from codepen

--- app.jsx
+++ app.jsx
@@ -14,17 +14,44 @@ class Pad extends React.Component{
   }
   
   handleResult(){
-    let currentResult = 0;
-    let h = this.state.history;
-    h.map((data,i) => {
-      console.log(data);
-      if(isNaN(data)){
-        if(data == "*" || data == "/"){
-          console.log(
-            h[i - 1] + data + h[i + 1]
-          );
-        }
+    let h = this.state.history.slice(), flag = false;
+    do{
+      console.log(h);
+      if(h.indexOf("*") || h.indexOf("/")){
+        h.map((data,i) => {
+          let fVal = h[i - 1], lVal = (i === (h.length - 1)) ? this.state.currentDisplay : h[i + 1];
+          if(isNaN(data) && (data == "*" || data == "/")){
+            if(data == "*"){
+              h.splice(i - 1, 3, (parseFloat(fVal) * parseFloat(lVal)));
+            }else{
+              h.splice(i - 1, 3, (parseFloat(fVal) / parseFloat(lVal)));
+            }
+          }
+        });
+      }else{
+        flag = false;
       }
+      console.log(h);
+    }while(flag && h.length > 0);
+    do{
+      console.log(h);
+      if(h.indexOf("+") || h.indexOf("-")){
+        h.map((data,i) => {
+          let fVal = h[i - 1], lVal = (i === (h.length - 1)) ? this.state.currentDisplay : h[i + 1];
+          if(isNaN(data) && (data == "+" || data == "-")){
+            h.splice(i, 2, parseFloat(lVal) * ((data == "-") ? -1 : 1));
+          }else{
+            h.splice(i, 1, parseFloat(data));
+          }
+        });
+      }else{
+        flag = false;
+      }
+      console.log(h);
+    }while(flag && h.length > 0);
+    this.setState({
+      history: [...this.state.history, this.state.currentDisplay],
+      currentDisplay: h.reduce((a, b) => a + b)
     })
   }
   
@@ -62,7 +89,7 @@ class Pad extends React.Component{
   render(){
     //console.log(this.state.history);
     return(
-      <div class="bg-dark p-2">
+      <div className="bg-dark p-2">
         <div id="history">
           {
             this.state.history.map((n) => n + " ")
@@ -74,25 +101,25 @@ class Pad extends React.Component{
         <div>
           <button id="clear" onClick={this.resetState}>C</button>
         </div>
-        <div class="d-flex">
+        <div className="d-flex">
           <button id="seven" onClick={this.handleClick} value="7">7</button>
           <button id="eight" onClick={this.handleClick} value="8">8</button>
           <button id="nine" onClick={this.handleClick} value="9">9</button>
           <button id="add" onClick={this.handleClick} value="+">+</button>
         </div>
-        <div class="d-flex">
+        <div className="d-flex">
           <button id="four" onClick={this.handleClick} value="4">4</button>
           <button id="five" onClick={this.handleClick} value="5">5</button>
           <button id="six" onClick={this.handleClick} value="6">6</button>
           <button id="substract" onClick={this.handleClick} value="-">-</button>
         </div>
-        <div class="d-flex">
+        <div className="d-flex">
           <button id="one" onClick={this.handleClick} value="1">1</button>
           <button id="two" onClick={this.handleClick} value="2">2</button>
           <button id="three" onClick={this.handleClick} value="3">3</button>
           <button id="multiply" onClick={this.handleClick} value="*">x</button>
         </div>
-        <div class="d-flex">
+        <div className="d-flex">
           <button id="zero" onClick={this.handleClick} value="0">0</button>
           <button id="decimal" onClick={this.handleClick} value=".">.</button>
           <button id="equals" onClick={this.handleResult} value="=">=</button>
@@ -113,8 +140,8 @@ class Main extends React.Component{
   
   render(){
     return(
-      <div class="w-100 h-100 d-flex justify-content-center align-items-center">
-        <div class="border border-secondary">
+      <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+        <div className="border border-secondary">
           <div>
             <Pad />
           </div>
